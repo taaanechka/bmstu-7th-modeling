48091718e35c2e53a366866600f35f2c731b8e71
Suchkova Tatiana <tanya18112001@mail.ru>
2023-02-12 19:00:13 +0300

    [4] Add lab_04

---
+++ lab_04/docs/rpz/inc/lst/distribute.py
@@ -0,0 +1,18 @@
+import numpy.random as nprand
+import random as rand
+
+class UniformDistribution:
+    def __init__(self, a: float, b: float):
+        self.a = a
+        self.b = b
+
+    def generate(self):
+        return self.a + (self.b - self.a) * rand.random()
+
+
+class PoissonDistribution:
+    def __init__(self, lam : float):
+        self.lam = lam
+
+    def generate(self):
+        return nprand.poisson(self.lam)
---
+++ lab_04/docs/rpz/inc/lst/event_model.py
@@ -0,0 +1,53 @@
+from random import randint
+
+def event_model(generator, processor, total_tasks=0, repeat=0):
+    done_tasks = 0
+    cur_qlen = max_qlen = 0
+    events = [[generator.generate(), 'g']]
+    free, process_flag = True, False
+
+    while done_tasks < total_tasks:
+        event = events.pop(0)
+
+        # Генератор
+        if event[1] == 'g':
+            cur_qlen += 1
+            if cur_qlen > max_qlen:
+                max_qlen = cur_qlen
+
+            add_event(events, [event[0] + generator.generate(), 'g'])
+
+            if free:
+                process_flag = True
+
+        # Обработчик
+        elif event[1] == 'p':
+            done_tasks += 1
+            if randint(1, 100) <= repeat:
+                cur_qlen += 1
+
+            process_flag = True
+
+        if process_flag:
+            if cur_qlen > 0:
+                cur_qlen -= 1
+                add_event(events, [event[0] + processor.generate(), 'p'])
+                free = False
+            else:
+                free = True
+
+            process_flag = False
+
+    return max_qlen
+
+
+def add_event(events, event: list):
+    i = 0
+
+    while i < len(events) and events[i][0] < event[0]:
+        i += 1
+
+    if 0 < i < len(events):
+        events.insert(i - 1, event)
+    else:
+        events.insert(i, event)
